ec790cdc99ab9e0991fc010e7bffaa396bc69b6b
Luisa Braga <mlpb@icomp.ufam.edu.br>
2020-06-10 21:38:39 -0400

    feat: login/signup page

---
+++ web/src/pages/Sign/index.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { FiLogIn, FiInfo } from 'react-icons/fi';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import Comment from '../../components/Comment';
+
+import './styles.css';
+
+import logo from '../../assets/logo.png';
+// interface HeaderProps {
+//   title: string;
+// }
+
+const Sign: React.FC = (props) => {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const handleLogin = async (event: FormEvent) => { }
+  const handleSubmit = async (event: FormEvent) => { }
+
+
+  return (
+    <div id='page-sign'>
+      <div className='content'>
+        <header>
+          <Link to='/'>
+            <img className='logo' src={logo} alt='Detector de Sexismo' />
+          </Link>
+        </header>
+        <form onSubmit={handleLogin}>
+          {showLogin ? (
+            <fieldset>
+              <legend>
+                <h2>Login</h2>
+              </legend>
+              <div className="field">
+                <label htmlFor="email"> E-mail</label>
+                <input
+                  type='text'
+                  name='email'
+                  id='email'
+                  onChange={() => { }}
+                />
+              </div>
+              <div className="field">
+                <label htmlFor="password"> Senha</label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  onChange={() => { }}
+                />
+              </div>
+            </fieldset>
+          ) : (
+              <fieldset>
+                <legend>
+                  <h2>Login</h2>
+                </legend>
+                <div className="field">
+                  <label htmlFor="name"> Nome</label>
+                  <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    onChange={() => { }}
+                  />
+                </div>
+                <div className="field">
+                  <label htmlFor="email"> E-mail</label>
+                  <input
+                    type='text'
+                    name='email'
+                    id='email'
+                    onChange={() => { }}
+                  />
+                </div>
+                <div className="field">
+                  <label htmlFor="password"> Senha</label>
+                  <input
+                    type='password'
+                    name='password'
+                    id='password'
+                    onChange={() => { }}
+                  />
+                </div>
+              </fieldset>
+            )}
+          <Link to='/main' className='button'>
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Entrar</strong>
+          </Link>
+          <a className='button' onClick={() => { setShowLogin(!showLogin) }}>
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Entrar</strong>
+          </a>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Sign;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Sign from './pages/Sign';
+
 import Main from './pages/Main';
 
 // import CreatePoint from './pages/CreatePoint';
@@ -13,6 +15,7 @@ const Routes = () => {
       <Route component={Home} path='/' exact />
       <Route component={About} path='/about' exact />
       {/* TODO: fazer private route */}
+      <Route component={Sign} path='/sign' exact />
       <Route component={Main} path='/main' exact />
 
       {/* <Route component={CreatePoint} path='/create-point' /> */}
